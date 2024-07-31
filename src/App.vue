699684d1967c997f2b835d016801562b84981fee
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-31 15:06:28 +0200

    [UPD] added function to retrieve data from API call

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script>
 import appHeader from './components/appHeader.vue';
+import { store } from './store.js'
+import axios from 'axios'
 
 export default {
   components: {
     appHeader
+  },
+  created() {
+
+  },
+  methods: {
+    getCardsList() {
+      axios.get(store.apiUrl).then((result) => {
+        store.cardsList = result.data.data
+      })
+    }
+  },
+  data() {
+    return {
+      store
+    }
   }
 }
 </script>
