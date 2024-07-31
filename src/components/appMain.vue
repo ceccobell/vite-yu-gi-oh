77d8af073d1f7cca8ef4f1db19799663c66e769d
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-31 16:36:34 +0200

    [UPD] added a new cards component that takes data with props

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,7 +1,11 @@
 <script>
 import { store } from '../store.js'
+import cards from './cards.vue'
 
 export default {
+    components: {
+        cards
+    },
     data() {
         return {
             store
@@ -13,18 +17,11 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-3 text-center mb-3" v-for="card, index in store.cardsList" :key="index">
-                <img :src="card.card_images[0].image_url_small" alt="">
-                <h4>{{ card.name }}</h4>
-                <div>{{ card.archetype }}</div>
-            </div>
+            <cards :cards="store.cardsList" />
         </div>
     </div>
 </template>
 
 <style>
-    img {
-        width: 100%;
-        height: auto;
-    }
+    
 </style>
---
+++ src/components/cards.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    props: {
+        cards: Array
+    }
+}
+</script>
+
+<template>
+    <div class="col-3 text-center mb-3" v-for="card, index in cards" :key="index">
+        <img :src="card.card_images[0].image_url_small" alt="">
+        <h4>{{ card.name }}</h4>
+        <div>{{ card.archetype }}</div>
+    </div>
+</template>
+
+<style scoped>
+    img {
+        width: 100%;
+        height: auto;
+    }
+</style>
