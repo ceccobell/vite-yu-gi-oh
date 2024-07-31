25009e18cb27ef47d7944a203c661c8d26c7f817
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-31 14:41:50 +0200

    [UPD] imported bootstrap and component appHeader

---
+++ src/components/appHeader.vue
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 d-flex p-3">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpXw-NuLf9rw9G_6TaJ40-uAxlX5LUBNy94Q&s" alt="">
+        <h1>Yu-Gi-Oh API</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 100px;
+  height: auto;
+}
+</style>
