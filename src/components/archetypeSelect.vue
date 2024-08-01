5f1e348e8dd3ed5a6b343c88786b53c2c508ba71
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-08-01 14:52:20 +0200

    [UPD] created component archetypeSelect.vue

---
+++ src/components/archetypeSelect.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div class="col-6 d-flex align-items-center">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>select archetype</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
